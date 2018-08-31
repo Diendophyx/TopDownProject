d945e96f795826140e5674e06b2792a7dc011953
Diendophyx <42424060+Diendophyx@users.noreply.github.com>
2018-09-01 00:09:29 +1000

    Initial Publish

---
+++ Top down Project/Assets/Scripts/Enemies.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemies : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform target;
+    public float speed;
+
+    private float distToTarget;
+    GameObject detectedPlayer;
+    bool playerInRange;
+    float timer;
+
+    void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        Seek();
+    }
+
+    private void Update()
+    {
+        Seek();
+    }
+
+    void Seek()
+    {
+        agent.SetDestination(target.position);
+    }
+}
